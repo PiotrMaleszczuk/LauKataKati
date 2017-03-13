9566468290e885063b29a2a892e46fa29d64077f
PiotrMaleszczuk <piotr@codeaddict.pl>
2017-03-13 23:58:59 +0100

    Unity Project Base

---
+++ Assets/_Scripts/App.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class App : MonoBehaviour {
+	
+    public static App Instance;
+    
+    [Serializable]
+    public class View {
+
+    }
+
+    [Serializable]
+    public class Model {
+        
+    }
+
+    [Serializable]
+    public class Controller {
+        
+    }
+
+    public Model model;
+    public View view;
+    public Controller controller;
+
+
+    void Awake() {
+        App.Instance = this;
+    }
+
+}
