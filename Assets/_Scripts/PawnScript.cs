21e8912d22ed7faf1819202d20b73ed7073309af
PBurdelak <pburdelak@gmail.com>
2017-04-04 21:39:27 +0200

    Prefabs_pawns_+_script

---
+++ Assets/_Scripts/PawnScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnScript : MonoBehaviour {
+
+	public int id;
+	public int team;
+	public int matrix_x;
+	public int matrix_y;
+	// Use this for initialization
+	public void Init ()
+	{
+		
+	}
+
+}
