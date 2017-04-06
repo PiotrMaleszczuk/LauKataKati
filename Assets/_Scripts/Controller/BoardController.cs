6761ec7c4bfac68d8afd9ce86100a844b39331d6
PBurdelak <pburdelak@gmail.com>
2017-04-07 00:55:06 +0200

    connecting_logic+glowsController+jumps

--- Assets/_Scripts/Controller/BoardController.cs
+++ Assets/_Scripts/Controller/BoardController.cs
@@ -6,41 +6,49 @@ public class BoardController : MonoBehaviour
 {
 
 	private App app;
-    private int[][] board;
+	private int[][] board;
 
-    private GameObject pawn1;
+	private GameObject pawn1;
 	private GameObject pawn2;
-    private GameObject empty;
-    private GameObject glow;
+	private GameObject empty;
+	private GameObject glow;
 
 	private Transform[] pointArray;
 	private Transform points;
-    private Transform boardTransform;
-    private Transform emptyTransform;
-    private Transform glowTransform;
+	private Transform boardTransform;
+	private Transform emptyTransform;
+	private Transform glowTransform;
 
-    private GameObject[] pawnsArray;
-    private GameObject[] emptyArray;
-    private GameObject[] glowsArray;
+	private GameObject[] pawnsArray;
+	private GameObject[] emptyArray;
+	[HideInInspector]
+	public GameObject[] glowsArray;
 
-	public void Init (){
+	private GlowController glowController;
+
+	PawnScript ps_chosen;
+	List<int[]> moves_list;
+
+	public void Init ()
+	{
 		app = App.Instance;
 		board = new int[3][];
 		boardTransform = app.view.board;
-        emptyTransform = app.view.empty;
-        glowTransform = app.view.glow;
-        glow = app.model.glows;
-        pawn1 = app.model.pawn1;
+		emptyTransform = app.view.empty;
+		glowTransform = app.view.glow;
+		glowController = app.controller.glow;
+		glow = app.model.glows;
+		pawn1 = app.model.pawn1;
 		pawn2 = app.model.pawn2;
-        empty = app.model.empty;
+		empty = app.model.empty;
 		points = app.view.points;
 		string boardString = "";
 		pointArray = new Transform[points.childCount];
 		pawnsArray = new GameObject[points.childCount];
-        emptyArray = new GameObject[points.childCount];
-        glowsArray = new GameObject[points.childCount];
+		emptyArray = new GameObject[points.childCount];
+		glowsArray = new GameObject[points.childCount];
 
-        for (int i = 0; i < points.childCount; i++) {
+		for (int i = 0; i < points.childCount; i++) {
 			pointArray [i] = points.GetChild (i);
 		}
 
@@ -49,75 +57,72 @@ public class BoardController : MonoBehaviour
 		for (int i = 0; i < board.Length; i++) {
 			board [i] = new int[7];
 			for (int j = 0; j < board [i].Length; j++) {
-                GameObject empty_position = Instantiate(empty);
-                empty_position.name = "Empty " + iterPos;
-                empty_position.transform.SetParent(emptyTransform);
-                empty_position.transform.localPosition = new Vector3(pointArray[iterPos].localPosition.x, pointArray[iterPos].localPosition.y, 1);
-                empty_position.AddComponent<CircleCollider2D>();
-                emptyArray[iterPos] = empty_position;
-                PawnScript emptyScript = emptyArray[iterPos].GetComponent<PawnScript>();
-                emptyScript.id = iterPos;
-                emptyScript.team = 0;
-                emptyScript.matrix_x = i;
-                emptyScript.matrix_y = j;
+				GameObject empty_position = Instantiate (empty);
+				empty_position.name = "Empty " + iterPos;
+				empty_position.transform.SetParent (emptyTransform);
+				empty_position.transform.localPosition = new Vector3 (pointArray [iterPos].localPosition.x, pointArray [iterPos].localPosition.y, 1);
+				empty_position.AddComponent<CircleCollider2D> ();
+				emptyArray [iterPos] = empty_position;
+				PawnScript emptyScript = emptyArray [iterPos].GetComponent<PawnScript> ();
+				emptyScript.id = iterPos;
+				emptyScript.team = 0;
+				emptyScript.matrix_x = i;
+				emptyScript.matrix_y = j;
 
-                GameObject glowPosition = Instantiate(glow);
-                glowPosition.name = "Glow " + iterPos;
-                glowPosition.transform.SetParent(glowTransform);
-                glowPosition.transform.localPosition = new Vector3(pointArray[iterPos].localPosition.x, pointArray[iterPos].localPosition.y, 0f);
-                glowsArray[iterPos] = glowPosition;
-                SpriteRenderer renderer = glowsArray[iterPos].GetComponent<SpriteRenderer>();
-                renderer.color = new Color(0f, 1f, 0f, 0f);
+				ps_chosen = null;
+				moves_list = new List<int[]> ();
 
-                if (i == 1 && j == 3) {
-                    board[i][j] = 0;
-                    boardString += board[i][j];
-                    iterPos++;
-                }
-                else if (i % 2 == 0 && j == 3) {
-                    board[i][j] = -1;
-                    boardString += board[i][j];
-                } else {
-                    if (i == 1 && j == 1) {
-                        board[i][j] = 0;
-                        boardString += board[i][j];
-                        iterPos++;
-                    } else {
-                        if (j <= 2) {
-                            board[i][j] = 1;
-                            boardString += board[i][j];
-                            GameObject pawn = Instantiate(pawn1);
-                            pawn.name = "Pawn " + iterPawn;
-                            pawn.transform.SetParent(boardTransform);
-                            pawn.transform.localPosition = new Vector3(pointArray[iterPos].localPosition.x, pointArray[iterPos].localPosition.y, 0f);
-                            pawn.AddComponent<CircleCollider2D>();
-                            pawnsArray[iterPawn] = pawn;
-                            PawnScript pawnScript = pawnsArray[iterPawn].GetComponent<PawnScript>();
-                            pawnScript.id = iterPawn;
-                            pawnScript.team = 1;
-                            pawnScript.matrix_x = i;
-                            pawnScript.matrix_y = j;
-                            iterPawn++;
-                            iterPos++;
-                        } else {
-                            board[i][j] = 2;
-                            boardString += board[i][j];
-                            GameObject pawn = Instantiate(pawn2);
-                            pawn.name = "Pawn " + iterPawn;
-                            pawn.transform.SetParent(boardTransform);
-                            pawn.transform.localPosition = new Vector3(pointArray[iterPos].localPosition.x, pointArray[iterPos].localPosition.y, 0f);
-                            pawn.AddComponent<CircleCollider2D>();
-                            pawnsArray[iterPawn] = pawn;
-                            PawnScript pawnScript = pawnsArray[iterPawn].GetComponent<PawnScript>();
-                            pawnScript.id = iterPawn;
-                            pawnScript.team = 2;
-                            pawnScript.matrix_x = i;
-                            pawnScript.matrix_y = j;
-                            iterPawn++;
-                            iterPos++;
-                        }
-                    }
-                }
+				GameObject glowPosition = Instantiate (glow);
+				glowPosition.name = "Glow " + iterPos;
+				glowPosition.transform.SetParent (glowTransform);
+				glowPosition.transform.localPosition = new Vector3 (pointArray [iterPos].localPosition.x, pointArray [iterPos].localPosition.y, 0f);
+				glowsArray [iterPos] = glowPosition;
+				SpriteRenderer renderer = glowsArray [iterPos].GetComponent<SpriteRenderer> ();
+				renderer.color = new Color (0f, 1f, 0f, 0f);
+
+				if (i == 1 && j == 3) {
+					board [i] [j] = 0;
+					boardString += board [i] [j];
+					iterPos++;
+				} else if (i % 2 == 0 && j == 3) {
+					board [i] [j] = -1;
+					boardString += board [i] [j];
+				} else {
+					if (j <= 2) {
+						board [i] [j] = 1;
+						boardString += board [i] [j];
+						GameObject pawn = Instantiate (pawn1);
+						pawn.name = "Pawn " + iterPawn;
+						pawn.transform.SetParent (boardTransform);
+						pawn.transform.localPosition = new Vector3 (pointArray [iterPos].localPosition.x, pointArray [iterPos].localPosition.y, 0f);
+						pawn.AddComponent<CircleCollider2D> ();
+						pawnsArray [iterPawn] = pawn;
+						PawnScript pawnScript = pawnsArray [iterPawn].GetComponent<PawnScript> ();
+						pawnScript.id = iterPawn;
+						pawnScript.team = 1;
+						pawnScript.matrix_x = i;
+						pawnScript.matrix_y = j;
+						iterPawn++;
+						iterPos++;
+					} else {
+						board [i] [j] = 2;
+						boardString += board [i] [j];
+						GameObject pawn = Instantiate (pawn2);
+						pawn.name = "Pawn " + iterPawn;
+						pawn.transform.SetParent (boardTransform);
+						pawn.transform.localPosition = new Vector3 (pointArray [iterPos].localPosition.x, pointArray [iterPos].localPosition.y, 0f);
+						pawn.AddComponent<CircleCollider2D> ();
+						pawnsArray [iterPawn] = pawn;
+						PawnScript pawnScript = pawnsArray [iterPawn].GetComponent<PawnScript> ();
+						pawnScript.id = iterPawn;
+						pawnScript.team = 2;
+						pawnScript.matrix_x = i;
+						pawnScript.matrix_y = j;
+						iterPawn++;
+						iterPos++;
+					}
+
+				}
 				boardString += " ";
 			}
 			boardString += "\n";
@@ -125,14 +130,49 @@ public class BoardController : MonoBehaviour
 		print (boardString);
 	}
 
-	public void Click (int team, int matrix_x, int matrix_y){
-        print("team: " + team + " x: " + matrix_x + " y: " + matrix_y);
-        if (team != 0) {
-            List<int[]> tmp = new List<int[]>();
-            tmp = app.controller.logic.checking(team, matrix_x, matrix_y, board);
-            for (int i = 0; i < tmp.Count; i++) {
-                print("Logic = x: " + tmp[i][0] + " y: " + tmp[i][1]);
-            }
-        }
+	public void Click (PawnScript pawnScript)
+	{
+		print ("team: " + pawnScript.team + " x: " + pawnScript.matrix_x + " y: " + pawnScript.matrix_y);
+		if (ps_chosen == null && pawnScript.team != 0) {
+			ps_chosen = pawnScript;
+			print ("Chosen pawn: " + ps_chosen.id);
+			moves_list = app.controller.logic.checking (pawnScript.team, pawnScript.matrix_x, pawnScript.matrix_y, board);
+			for (int i = 0; i < moves_list.Count; i++) {
+				print ("Logic = x: " + moves_list [i] [0] + " y: " + moves_list [i] [1]);
+			}
+			glowController.Glowing (ps_chosen, moves_list);
+		} else if (ps_chosen == pawnScript && pawnScript.team != 0) {
+			print ("Unchosen pawn: " + pawnScript.id);
+			ps_chosen = null;
+			moves_list.Clear ();
+			glowController.DisableGlows ();
+		} else if (pawnScript.team == 0 && ps_chosen != null) {
+			for (int i = 0; i < moves_list.Count; i++) {
+				if (pawnScript.matrix_x == moves_list [i] [0] && pawnScript.matrix_y == moves_list [i] [1]) {
+					if (app.controller.logic.capture) {
+						int captured_x = ps_chosen.matrix_x + (pawnScript.matrix_x - ps_chosen.matrix_x) / 2;
+						int captured_y = ps_chosen.matrix_y + (pawnScript.matrix_y - ps_chosen.matrix_y) / 2;
+						for (int j = 0; i < pawnsArray.Length; j++) {
+							PawnScript ps_captured = pawnsArray [j].GetComponent<PawnScript> ();
+							if (ps_captured.matrix_x == captured_x && ps_captured.matrix_y == captured_y) {
+								ps_captured.gameObject.SetActive (false);
+								board [captured_x] [captured_y] = 0;
+								break;
+							}
+						}
+					}
+					board [ps_chosen.matrix_x] [ps_chosen.matrix_y] = 0;
+					ps_chosen.transform.localPosition = new Vector3 (pawnScript.transform.localPosition.x, pawnScript.transform.localPosition.y, 0f);
+					board [pawnScript.matrix_x] [pawnScript.matrix_y] = ps_chosen.team;
+					ps_chosen.matrix_x = pawnScript.matrix_x;
+					ps_chosen.matrix_y = pawnScript.matrix_y;
+					ps_chosen = null;
+					moves_list.Clear ();
+					glowController.DisableGlows ();
+					break;
+
+				}
+			}
+		}
 	}
 }
--- Assets/_Scripts/Controller/GlowController.cs
+++ Assets/_Scripts/Controller/GlowController.cs
@@ -2,65 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlowController : MonoBehaviour {
+public class GlowController : MonoBehaviour
+{
 
-    private App app;
-    private int[][] board;
-    private readonly Color GREEN = new Color(0f, 1f, 0f, 1f);
-    private readonly Color BLUE = new Color(0f, 0f, 1f, 1f);
-    private readonly Color HIDDEN = new Color(0f, 0f, 0f, 0f);
-    private Transform boardTransform;
+	private App app;
+	private int[][] board;
+	private readonly Color GREEN = new Color (0f, 1f, 0f, 1f);
+	private readonly Color BLUE = new Color (0f, 0f, 1f, 1f);
+	private readonly Color HIDDEN = new Color (0f, 0f, 0f, 0f);
+	private Transform boardTransform;
     
-    private GameObject[] glowsArray;
+	private GameObject[] glowsArray;
 
-    public void Init() {
-        app = App.Instance;
-        boardTransform = app.view.board;
-        board = new int[3][];
+	public void Init ()
+	{
+		app = App.Instance;
+		boardTransform = app.view.board;
+		board = new int[3][];
+		glowsArray = app.controller.board.glowsArray;
 
-        for (int i=0; i<board.Length; i++) {
-            board[i] = new int[7];
-            for (int j=0; j<board[i].Length; i++) {
-                if (i % 2 == 0 && j == 3) {
-                    board[i][j] = -1;
-                } else {
-                    board[i][j] = 0;
-                }
-            }
-        }
-    }
+		string boardString = "";
+		for (int i = 0; i < board.Length; i++) {
+			board [i] = new int[7];
+			for (int j = 0; j < board [i].Length; j++) {
+				if (i % 2 == 0 && j == 3) {
+					board [i] [j] = -1;
+					boardString += board [i] [j];
+				} else {
+					board [i] [j] = 0;
+					boardString += board [i] [j];
+				}
+				boardString += " ";
+			}
+			boardString += "\n";
+		}
+		print (boardString);
+	}
 
-    public void Glowing(PawnScript pS, List<int[]> moves_list) {
-        List<int[]> moves = moves_list;
+	public void Glowing (PawnScript pS, List<int[]> moves_list)
+	{
+		print (pS.matrix_x + "  " + pS.matrix_y);
+		List<int[]> moves;
+		moves = new List<int[]> ();
+		for (int i = 0; i < moves_list.Count; i++) {
+			moves.Add (moves_list [i]);
+		}
 
-        int iter = 0;
-        for (int i=0; i<board.Length; i++) {
-            for (int j=0; j<board[i].Length; j++) {
-                if (board[i][j] == -1) {
-                    continue;
-                }
+		int iter = 0;
 
-                if(pS.matrix_x==i && pS.matrix_y == j) {
-                    glowsArray[iter].GetComponent<SpriteRenderer>().color = BLUE;
-                    iter++;
-                    continue;
-                }
+		for (int i = 0; i < board.Length; i++) {
+			for (int j = 0; j < board [i].Length; j++) {
+				if (board [i] [j] == -1) {
+					continue;
+				}
 
-                for(int k=0; k<moves.Count; k++) {
-                    if(moves[k][0] == i && moves[k][1] == j) {
-                        glowsArray[iter].GetComponent<SpriteRenderer>().color = GREEN;
-                        iter++;
-                        moves.RemoveAt(k);
-                        continue;
-                    }
-                }
-            }
-        }
-    }
+				if (pS.matrix_x == i && pS.matrix_y == j) {
+					glowsArray [iter].GetComponent<SpriteRenderer> ().color = BLUE;
+					iter++;
+					continue;
+				}
 
-    public void DisableGlows(GameObject[] glowsArray) {
-        for(int i=0; i<glowsArray.Length; i++) {
-            glowsArray[i].GetComponent<SpriteRenderer>().color = HIDDEN;
-        }
-    }
+				for (int k = 0; k < moves.Count; k++) {
+					if (moves [k] [0] == i && moves [k] [1] == j) {
+						glowsArray [iter].GetComponent<SpriteRenderer> ().color = GREEN;
+						moves.RemoveAt (k);
+						break;
+					}
+				}
+				iter++;
+			}
+		}
+	}
+
+	public void DisableGlows ()
+	{
+		for (int i = 0; i < glowsArray.Length; i++) {
+			glowsArray [i].GetComponent<SpriteRenderer> ().color = HIDDEN;
+		}
+	}
 }
